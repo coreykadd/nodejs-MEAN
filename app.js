a803679a22a16be3bc8cb84a4ce3a67fb5e1a459
coreykadd <coreymangan22@live.ie>
2018-02-07 09:42:42 +0000

    Can enter own post title, validation for post title

--- app.js
+++ app.js
@@ -12,5 +12,14 @@ app.controller('MainCtrl', [
             {title: 'Post 4', upvotes: 8},
             {title: 'Post 5', upvotes: 3}
         ];
+
+        $scope.addPost = function(){
+            if(!$scope.title || $scope.title === ''){
+                alert("Please enter title for post");
+                return;
+            }
+            $scope.posts.push({title: $scope.title, upvotes: 0});
+            $scope.title = '';
+        };
     }
 ])
